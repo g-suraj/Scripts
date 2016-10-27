8ebe5c494414496a813522d42f5c13bf8e920d2b
sg6215 <sg6215@ic.ac.uk>
2016-10-27 22:06:40 +0100

    Slightly modified Readme and Removed coloring from WifiScript

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# WifiScript
-Tiny script to output a colorised wifi status to the status bar
+#Scripts 
+Tiny scripts to provide various functionalities to status bars.
+
+* WifiScript - output status of wifi with nmcli output to toggle between three states: Home, Work or Disconnected. TODO: add a click functionality to launch command "sudo service restart networkmanager" for XPS 13 wifi chip instability issues.
--- WifiScript.sh
+++ WifiScript.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-#Output Format:  Connected/Disconnected to Home/School
-N=7
-C=#fb4934
-STATUS="$(echo `nmcli g` | cut -d " " -f $N | sed -e "s/\b\(.\)/\u\1/g")"
+STATUS="$(echo `nmcli g` | cut -d " " -f 7 | sed -e "s/\b\(.\)/\u\1/g")"
 if [[ $STATUS == *"Connected"* ]]; then
   STATUS="$(echo `nmcli d` | cut -d " " -f 8)"
-  C=#8ec07c
   if [[ $STATUS == *"Imperial-WPA"*  ]]; 
   then
     STATUS=Imperial
-    #statements
   else
     STATUS=Home
   fi
 fi
-echo "%{F$C}  $STATUS"
+echo "  $STATUS"
