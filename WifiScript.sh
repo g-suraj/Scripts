762e2392cf66120b564c52867a447cacb24617a2
sg6215 <sg6215@ic.ac.uk>
2016-10-27 17:38:37 +0100

    Added ife to determind if at home or work

--- WifiScript.sh
+++ WifiScript.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #Output Format:  Connected/Disconnected to Home/School
 N=7
+C=#fb4934
 STATUS="$(echo `nmcli g` | cut -d " " -f $N | sed -e "s/\b\(.\)/\u\1/g")"
-echo "  $STATUS"
+if [[ $STATUS == *"Connected"* ]]; then
+  STATUS="$(echo `nmcli d` | cut -d " " -f 8)"
+  C=#ebdbb2
+fi
+echo "%{F$C}  $STATUS"
